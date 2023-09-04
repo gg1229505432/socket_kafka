660444c3fa032ee79d8a3aa9172e4180b6e31c1c
FRANKSTEIN11 <gg1229505432@gmail.com>
2023-09-05 00:31:08 +0800

    add .gitignore

--- src/main/java/com/yenanren/socket_kafka/client/KafkaProducerExample.java
+++ src/main/java/com/yenanren/socket_kafka/client/KafkaProducerExample.java
@@ -8,9 +8,9 @@ import java.util.Properties;
 public class KafkaProducerExample {
     public static void main(String[] args) {
         Properties props = new Properties();
-        props.put("bootstrap.servers", KafkaConst.KAFKA_HOST);
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaConst.KAFKA_HOST);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
 
         Producer<String, String> producer = new KafkaProducer<>(props);
         for (int i = 0; i < 100; i++) {
